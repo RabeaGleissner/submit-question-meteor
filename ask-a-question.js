f0101f69b9276b34b61a34827b27df8215c230a2
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-02 15:44:42 +0000

    set up Mongo collection and user input form

--- ask-a-question.js
+++ ask-a-question.js
@@ -1,23 +1,17 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Questions = new Mongo.Collection("questions");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  if (Meteor.isClient) {
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+    Template.body.helpers({
+      questions: function () {
+        return Questions.find({});
+      }
+    });
+  }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
