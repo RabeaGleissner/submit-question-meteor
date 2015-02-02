f08a772aa467e2787e1bcf25c39fbfdc02735b43
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-02 18:10:25 +0000

    refactored to use methods

--- ask-a-question.js
+++ ask-a-question.js
@@ -10,27 +10,19 @@ Questions = new Mongo.Collection("questions");
 
     Template.body.events({
       "submit .new-question": function (event) {
-        event.preventDefault();
-        console.log('default prevented')
-
-       var text = event.target.text.value;
-        
-        Questions.insert({
-          text: text,
-          createdAt: new Date(),
-          owner: Meteor.userId(),
-          username: Meteor.user().username
-        });
-
-        event.target.text.value = "";
+      event.preventDefault();
+      var text = event.target.text.value;
+      Meteor.call("addQuestion", text);
+      event.target.text.value = "";
       }
     });
 
     Template.question.events({
       "click .delete": function () {
-        Questions.remove(this._id);
+    Meteor.call("deleteQuestion", this._id);
       }
     });
+
 
     Accounts.ui.config({
       passwordSignupFields: "USERNAME_ONLY"
@@ -38,9 +30,29 @@ Questions = new Mongo.Collection("questions");
 
   }
 
+  Meteor.methods({
+    addQuestion: function (text) {
+      // Make sure the user is logged in before inserting a task
+      if (! Meteor.userId()) {
+        throw new Meteor.Error("not-authorized");
+      }
+
+    Questions.insert({
+          text: text,
+          createdAt: new Date(),
+          owner: Meteor.userId(),
+          username: Meteor.user().username
+        });
+      },
+      deleteQuestion: function (questionId) {
+        Questions.remove(questionId);
+      }
+    });
 
 // if (Meteor.isServer) {
 //   Meteor.startup(function () {
 //     // code to run on server at startup
 //   });
 // }
+
+
